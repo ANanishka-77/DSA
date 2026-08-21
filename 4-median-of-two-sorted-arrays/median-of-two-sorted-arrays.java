b77859e57c2ca307b854b85dcb69815ed50c2cda
Anishka Gautam <gautamji860460@gmail.com>
2026-08-21 13:00:59 +0530

    Time: 8 ms (13.56%) | Memory: 48.6 MB (94.24%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,38 +1,64 @@
-class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+// class Solution {
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        int n = nums1.length + nums2.length;
+//         int n = nums1.length + nums2.length;
 
-        int[] nums3 = new int[n];
+//         int[] nums3 = new int[n];
 
-        int k = 0;
+//         int k = 0;
 
     
-        for (int i = 0; i < nums1.length; i++) {
-            nums3[k] = nums1[i];
-            k++;
-        }
+//         for (int i = 0; i < nums1.length; i++) {
+//             nums3[k] = nums1[i];
+//             k++;
+//         }
 
       
-        for (int i = 0; i < nums2.length; i++) {
-            nums3[k] = nums2[i];
-            k++;
-        }
+//         for (int i = 0; i < nums2.length; i++) {
+//             nums3[k] = nums2[i];
+//             k++;
+//         }
 
        
-        Arrays.sort(nums3);
+//         Arrays.sort(nums3);
 
   
-        if (n % 2 != 0) {
-            return nums3[n / 2];
-        }
+//         if (n % 2 != 0) {
+//             return nums3[n / 2];
+//         }
 
       
-        else {
-            int left = nums3[n / 2 - 1];
-            int right = nums3[n / 2];
+//         else {
+//             int left = nums3[n / 2 - 1];
+//             int right = nums3[n / 2];
 
-            return (left + right) / 2.0;
+//             return (left + right) / 2.0;
+//         }
+//     }
+// }
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
+        int[] nums3= new int[n+m];
+        for(int i=0;i<n;i++)
+        {
+            nums3[i]=nums1[i];
         }
+         for(int i=0;i<m;i++)
+      {
+            nums3[n+i]=nums2[i];
+        }
+        Arrays.sort(nums3);
+        int length = n + m;
+        if(length % 2 == 1) 
+        {
+            return nums3[length / 2];
+        }
+        else 
+        {
+             int sum= ((nums3[length / 2 - 1] )+ (nums3[length / 2]));
+             return sum/2.0;
     }
+}
 }
