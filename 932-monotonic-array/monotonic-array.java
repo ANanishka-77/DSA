e699ff9aa8e78bec0776227962fffa0d9ee93c4a
Anishka Gautam <gautamji860460@gmail.com>
2026-09-13 23:55:17 +0530

    Time: 2 ms (68.79%) | Memory: 85.3 MB (20.71%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+
+        boolean ascending = true;
+        boolean descending = true;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] > nums[i - 1]) {
+                descending = false;
+            }
+
+            if (nums[i] < nums[i - 1]) {
+                ascending = false;
+            }
+        }
+
+        if(ascending==true||descending==true)
+        return true;
+        return false;
+    }
+}
