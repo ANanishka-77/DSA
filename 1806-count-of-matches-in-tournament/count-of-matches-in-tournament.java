3e804803012fc0f7885280db86f054dd2ca4f829
Anishka Gautam <gautamji860460@gmail.com>
2026-09-16 21:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (71.87%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int sum=0;
+        while(n>1)
+        {
+       
+            if(n%2==0)
+            {
+                n=n/2;
+                sum=sum+n;
+            }
+            else{
+               int num=(n-1)/2;
+                sum=sum+num;
+                n=num+1;
+            }
+
+        }
+        return sum;
+    }
+}
