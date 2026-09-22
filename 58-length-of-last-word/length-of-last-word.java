0dfa28c51266148c79926b5c746630748b81f1d7
Anishka Gautam <gautamji860460@gmail.com>
2026-09-22 15:27:37 +0530

    Time: 1 ms (26.00%) | Memory: 43 MB (68.56%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,24 +1,48 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+
+//         char[] chars = s.toCharArray();
+//         int count = 0;
+//         int i = chars.length - 1;
+
+// while (i >= 0 && chars[i] == ' ') {
+//             i--;
+//         }        
+
+//         while (i >= 0 && chars[i] != ' ') {
+//             count++;
+//             i--;
+//         }
+
+//         return count;
+//     }
+// }
+
 class Solution {
     public int lengthOfLastWord(String s) {
-
-        char[] chars = s.toCharArray();
+        char[] Array = s.toCharArray();
+        int n = Array.length;
         int count = 0;
-        int i = chars.length - 1;
 
-      
-while (i >= 0 && chars[i] == ' ') {
-            i--;
-        }        
-      
-        while (i >= 0 && chars[i] != ' ') {
-            count++;
-            i--;
+        for (int i = 0; i < n; i++) {
+            if (Array[i] == ' ') {
+
+                if (count > 0) {
+                    int j = i + 1;
+                    while (j < Array.length && Array[j] == ' ') {
+                        j++;
+                    }
+                
+                if (j < Array.length) {
+                    count = 0;
+                }
+                }
+            } else {
+                count++;
+
+            }
+
         }
-
-
-
-
         return count;
     }
 }
-
