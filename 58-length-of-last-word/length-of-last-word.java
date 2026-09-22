e2901995299cdcb9ff5a7a381dcfa0e7dd785d49
Anishka Gautam <gautamji860460@gmail.com>
2026-09-22 15:08:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (6.02%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -6,16 +6,19 @@ class Solution {
         int i = chars.length - 1;
 
       
-        while (i >= 0 && chars[i] == ' ') {
+while (i >= 0 && chars[i] == ' ') {
             i--;
-        }
-
+        }        
       
         while (i >= 0 && chars[i] != ' ') {
             count++;
             i--;
         }
 
+
+
+
         return count;
     }
 }
+
