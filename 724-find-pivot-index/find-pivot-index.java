1d3078c24ad5952c5f673881293bec607a393284
Anishka Gautam <gautamji860460@gmail.com>
2026-09-26 10:36:54 +0530

    Time: 1 ms (96.15%) | Memory: 47.2 MB (77.73%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,36 +1,59 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
-        int sum2=0;
-        int[] sumLeft = new int[n];
-        int[] sumRight = new int[n];
+        // int n = nums.length;
+        // int sum = 0;
+        // int sum2=0;
+        // int[] sumLeft = new int[n];
+        // int[] sumRight = new int[n];
 
-        sumLeft[0]=nums[0];
+        // sumLeft[0]=nums[0];
 
-        for (int i = 1; i < n; i++) {
-            sum = nums[i] + sumLeft[i-1];
-            sumLeft[i] = sum;
-        }
+        // for (int i = 1; i < n; i++) {
+        //     sum = nums[i] + sumLeft[i-1];
+        //     sumLeft[i] = sum;
+        // }
        
 
-        sumRight[n-1]=nums[n-1];
-        for (int i = n - 2; i >= 0; i--) {
-            sum2 = nums[i] + sumRight[i+1];
-            sumRight[i] = sum2;
-        }
-        int i = 0;
-        int j = 0;
-        while (i < sumLeft.length && j < sumRight.length) {
+        // sumRight[n-1]=nums[n-1];
+        // for (int i = n - 2; i >= 0; i--) {
+        //     sum2 = nums[i] + sumRight[i+1];
+        //     sumRight[i] = sum2;
+        // }
+        // int i = 0;
+        // int j = 0;
+        // while (i < sumLeft.length && j < sumRight.length) {
 
-            if (sumLeft[i] == sumRight[j]) {
-                return i;
-            } else {
-                i++;
-                j++;
-            }
+        //     if (sumLeft[i] == sumRight[j]) {
+        //         return i;
+        //     } else {
+        //         i++;
+        //         j++;
+        //     }
 
-        }
-        return -1;
+        // }
+        // return -1;
+
+
+        int leftsum=0;
+         int totalsum=0;
+         for(int i=0;i<nums.length;i++)
+         {
+            totalsum=totalsum+nums[i];
+         }
+       
+       for(int i=0;i<nums.length;i++)
+       {
+         int rightsum=totalsum-leftsum-nums[i];
+
+         if(leftsum==rightsum)
+         {
+            return i;
+         }
+
+         leftsum=leftsum+nums[i];
+       }
+
+       return -1;
+
     }
 }
